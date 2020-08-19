ca74de4dce42507fa435d4aa0dee2d3677195ba6
Edcurrock <edgtg0304@gmail.com>
2020-08-19 22:59:00 +0300

    Implementing GOAP Scripts

---
+++ GOAP/Assets/Scripts/GAction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class GAction : MonoBehaviour
+{
+    public string actionName = "SimpleAction";
+    public float cost = 1.0f;
+    public GameObject target;
+    public GameObject targetTag;
+    public float duration = 0f; 
+    public WorldState[] preConditions;
+    public WorldState[] afterEffects;
+    public NavMeshAgent agent;
+
+    public Dictionary<string,int> preconditions;
+    public Dictionary<string,int> effects;
+    
+    public WorldState agentBeliefs;
+    public bool running = false;
+
+    public GAction()
+    {
+        preconditions = new Dictionary<string,int>();
+        effects = new Dictionary<string, int>();
+
+    }
+
+    private void Awake() 
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        if(preConditions != null)
+        {
+            foreach(WorldState wState in preConditions)
+            {
+                preconditions.Add(wState.key,wState.value);
+            }
+        }
+
+        if (afterEffects != null)
+        {
+            foreach (WorldState wState in afterEffects)
+            {
+                effects.Add(wState.key, wState.value);
+            }
+        }
+    }
+
+
+    public bool IsAchievable()
+    {
+        return true;
+    }
+
+    public bool IsAchievableGiven(Dictionary<string,int> conditions)
+    {
+        foreach(KeyValuePair<string,int> pair in preconditions)
+        {
+            if(!conditions.ContainsKey(pair.Key)) 
+                return false;
+        }   
+        return true;
+    }
+  
+  public abstract bool PrePerform();
+  public abstract bool PostPerform();
+}
