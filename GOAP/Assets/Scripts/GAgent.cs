d1f223960995ea6a5c012c6e98d958d22661dbde
Edcurrock <edgtg0304@gmail.com>
2020-08-20 20:39:12 +0300

    Add GPlanner.cs

--- GOAP/Assets/Scripts/GAgent.cs
+++ GOAP/Assets/Scripts/GAgent.cs
@@ -1,17 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;  
+
+public class SubGoal
+{
+    public Dictionary<string,int> sgoals;
+    public bool remove;
+
+    public SubGoal(string key, int value, bool rmv)
+    {
+        sgoals = new Dictionary<string, int>();
+        sgoals.Add(key,value);
+        remove = rmv;
+    }
+}
 
 public class GAgent : MonoBehaviour
 {
+    List<GAction> actions = new List<GAction>();
+    Dictionary<SubGoal,int> goals = new Dictionary<SubGoal, int>();
+
+    GPlanner planner;
+    Queue<GAction> actQueue;
+    public GAction currentAction;
+    SubGoal currentGoal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach(GAction act in acts)
+        {
+             actions.Add(act);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    void LateUpdate()
     {
         
     }
